454e5a6b231119568cadc38180126560894dacfc
saketh <saketh554>
2025-06-21 14:41:10 -0500

    updated home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,46 +1,43 @@
-import React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { name } from '../constants';
 import Footer from './Footer';
 
 const Home = () => {
+  const ref = useRef(0);
+  const [text, setText] = useState('');
 
-	const ref = useRef(0);
-	const [text, setText] = useState('');
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (ref.current < name.length) {
+        setText(prev => {
+          const next = prev + name[ref.current];
+          ref.current++;
+          return next;
+        });
+      }
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			if (ref.current < name.length) {
-				ref.current++;
-				setText(() => text + name[ref.current - 1]);
-			}
-		}, 500);
-		return () => clearInterval(interval);
-	}, [text]);
-
-	return (
-		<div className='area relative z-0 bg-black w-screen h-screen'>
-			<ul class="circles">
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-				<li></li>
-			</ul>
-			<div className='hero relative h-[calc(100vh)] flex justify-center items-center text-white' id='hero'>
-				<div className='pt-4 h-36 backdrop-blur-sm rounded-3xl'>
-					<h1 className='text-6xl sm:text-7xl font-extrabold mt-2'>Hi, I'm&nbsp;<span className='text-yellow-200 font-extrabold'>{text}</span></h1>
-					<p className='mt-3 text-xl'>I build scalable systems and optimize performance with full-stack expertise.</p>
-				</div>      
-			</div>
-			<Footer/>
-		</div>	
-	);
-}
+  return (
+    <div className='area relative z-0 bg-black w-screen h-screen'>
+      <ul className="circles">
+        <li></li><li></li><li></li><li></li><li></li>
+        <li></li><li></li><li></li><li></li><li></li>
+      </ul>
+      <div className='hero relative h-[calc(100vh)] flex justify-center items-center text-white' id='hero'>
+        <div className='pt-4 h-36 backdrop-blur-sm rounded-3xl'>
+          <h1 className='text-6xl sm:text-7xl font-extrabold mt-2'>
+            Hi, I'm&nbsp;<span className='text-yellow-200 font-extrabold'>{text}</span>
+          </h1>
+          <p className='mt-3 text-xl'>
+            I build scalable systems and optimize performance with full-stack expertise.
+          </p>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Home;
