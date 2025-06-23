61a98005dff7fa624324293096155e3e13d19490
saketh <saketh554>
2025-06-23 10:25:12 -0500

    Fixes css

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";  // ✅ Import App which uses HashRouter
+import "./index.css?v=2"; // cache-busting to prevent stale CSS
+import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
